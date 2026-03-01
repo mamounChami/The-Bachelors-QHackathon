22dc7926b6b05ebe683a541790de3d5df7342000
tekinalperen08 <tekinalperen08@gmail.com>
2026-03-01 03:43:39 +0100

    improvement on sampler and brute forcer

--- DataSet/generate_N.py
+++ DataSet/generate_N.py
@@ -1,6 +1,10 @@
 import numpy as np
 
-def generate_payment(x, int_threshold, prop):
+def generate_payment(int_threshold, prop):
+    loaded_pdf = np.load('kde_data.npz')
+    x = loaded_pdf['x_final']
+    pdf = loaded_pdf['y_final_per_year']
+
     output = np.zeros_like(x)
     for i in range(len(x)):
         if x[i] < 100_000_000 or x[i] > 500_000_000:
@@ -9,23 +13,23 @@ def generate_payment(x, int_threshold, prop):
             output[i] = x[i]
         else:
             output[i] = prop * x[i]
-    return output
+    return np.sum(output * pdf) * (x[1] - x[0])
 
-def generate_profit(x, payment, pdf, premium):
-    return np.sum((payment - x)*pdf)*(x[1] - x[0]) - premium
-
-def generate_N(N, props, thresholds, premiums):
+def expected_loss():
     loaded_pdf = np.load('kde_data.npz')
     x = loaded_pdf['x_final']
     y = loaded_pdf['y_final_per_year']
+    return np.sum(x * y) * (x[1] - x[0])
+
+def generate_N(N, props, thresholds, premiums):
+
     r_values = np.zeros((1, N))
     for i in range(N):
         prop = props[i]
         int_threshold = thresholds[i]
         premium = premiums[i]
         
-        payment = generate_payment(x, int_threshold, prop)
-        profit = generate_profit(x, payment, y, premium)
-        r_values[0, i] = profit
+        payment = generate_payment(int_threshold, prop)
+        r_values[0, i] = payment - premium
     
-    return r_values
+    return r_values, expected_loss()
